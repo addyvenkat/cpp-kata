025ec1ed5f113ab04c2758e52415ad6104cc23a0
Adithya Venkat Ramanan <addyvenkat@gmail.com>
2025-10-09 22:09:28 -0700

    MoveOnlyBuffer implemented with basic test

---
+++ moveOnlyBuffer/inc/moveOnlyBuffer.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+
+class CMoveOnlyBuffer
+{
+private:
+    uint32_t * _dataPtr{};
+public:
+    // Constructor
+    explicit CMoveOnlyBuffer(uint32_t val)
+    {
+        this->_dataPtr = new uint32_t(val);
+    }
+
+    // Move constructor
+    CMoveOnlyBuffer(CMoveOnlyBuffer&& other) noexcept
+    {
+        this->_dataPtr = other._dataPtr;
+        other._dataPtr = nullptr;
+    }
+
+    // Move assignment - default
+    CMoveOnlyBuffer& operator=(CMoveOnlyBuffer&& other) noexcept
+    {
+        if(this != &other)
+        {
+            delete this->_dataPtr;
+            this->_dataPtr = other._dataPtr;
+            other._dataPtr = nullptr;
+        }
+        return *this;
+    }
+    
+    // Destructor
+    ~CMoveOnlyBuffer() noexcept
+    {
+        delete this->_dataPtr;
+    }
+    
+    CMoveOnlyBuffer(const CMoveOnlyBuffer&) = delete; // Copy constructor - delete
+    CMoveOnlyBuffer& operator=(const CMoveOnlyBuffer&) = delete; // Copy assignment - delete 
+
+    uint32_t* getDataPtr() noexcept { return this->_dataPtr; }
+    uint32_t getVal() noexcept {return (_dataPtr) ? *_dataPtr : 0;}
+};
+
+static_assert(!std::is_copy_constructible_v<CMoveOnlyBuffer>); // Shouldn't be copy constructible
+static_assert(std::is_move_constructible_v<CMoveOnlyBuffer>); // Should be move constructible
---
+++ moveOnlyBuffer/test/testMoveOnlyBuffer.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "moveOnlyBuffer.hpp"
+
+TEST(MoveOnlyBuffer, Basic) {
+  CMoveOnlyBuffer b(256);
+  EXPECT_EQ(b.getVal(), 256);
+  EXPECT_NE(b.getDataPtr(), nullptr);
+}
+
+TEST(MoveOnlyBuffer, MoveSemantics) {
+  CMoveOnlyBuffer a(128);
+  auto p = a.getDataPtr();
+  CMoveOnlyBuffer b = std::move(a);
+  EXPECT_EQ(b.getVal(), 128);
+  EXPECT_EQ(b.getDataPtr(), p);
+}

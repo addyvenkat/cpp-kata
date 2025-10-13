065b22d00a0a3b6be9aa82772be37692ab658c1c
Adithya Venkat Ramanan <addyvenkat@gmail.com>
2025-10-12 18:41:26 -0700

    Implemented moveOnlyBuffer with unique_ptr instead of new/delete

--- moveOnlyBuffer/inc/moveOnlyBuffer.hpp
+++ moveOnlyBuffer/inc/moveOnlyBuffer.hpp
@@ -3,8 +3,44 @@
 #include <cstddef>
 #include <memory>
 
-// TODO: Re-attempt with unique_ptr instead of new/delete
+#ifdef USE_UNIQUE_PTR
+// Using unique_ptr
+class CMoveOnlyBuffer
+{
+private:
+    std::unique_ptr<uint32_t> _dataPtr;
+public:
+    // Constructor
+    explicit CMoveOnlyBuffer(uint32_t val)
+    {
+        this->_dataPtr = std::make_unique<uint32_t>(val);
+    }
 
+    // Move constructor
+    CMoveOnlyBuffer(CMoveOnlyBuffer&& other) noexcept
+    {
+        this->_dataPtr = std::move(other._dataPtr);
+    }
+
+    // Move assignment - default
+    CMoveOnlyBuffer& operator=(CMoveOnlyBuffer&& other) noexcept
+    {
+        this->_dataPtr = std::move(other._dataPtr);
+        return *this;
+    }
+    
+    // Destructor
+    ~CMoveOnlyBuffer() = default;
+    
+    CMoveOnlyBuffer(const CMoveOnlyBuffer&) = delete; // Copy constructor - delete
+    CMoveOnlyBuffer& operator=(const CMoveOnlyBuffer&) = delete; // Copy assignment - delete 
+
+    uint32_t* getDataPtr() noexcept { return this->_dataPtr.get(); }
+    uint32_t getVal() noexcept {return (_dataPtr) ? *_dataPtr : 0;}
+};
+
+#else
+// Using new-delete
 class CMoveOnlyBuffer
 {
 private:
@@ -47,6 +83,7 @@ public:
     uint32_t* getDataPtr() noexcept { return this->_dataPtr; }
     uint32_t getVal() noexcept {return (_dataPtr) ? *_dataPtr : 0;}
 };
+#endif // USE_UNIQUE_PTR
 
 static_assert(!std::is_copy_constructible_v<CMoveOnlyBuffer>); // Shouldn't be copy constructible
 static_assert(std::is_move_constructible_v<CMoveOnlyBuffer>); // Should be move constructible
